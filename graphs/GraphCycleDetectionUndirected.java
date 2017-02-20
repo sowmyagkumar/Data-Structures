5a2b7728510c4bd814229624eea96afcbd40e88c
Sowmya <sowmya.kumar@emc.com>
2017-02-20 19:49:06 +0530

    Cycle detection

--- graphs/GraphCycleDetectionUndirected.java
+++ graphs/GraphCycleDetectionUndirected.java
@@ -3,33 +3,44 @@ package graphs;
 import java.util.LinkedList;
 import java.util.Iterator;
 
-public class GraphCycleDetection{
+public class GraphCycleDetectionUndirected{
 
     static int vertices;
 	static LinkedList[] adj;
 	
 	public static void addEdge(int s, int e){
 	    adj[s].add(e);
+		adj[e].add(s);
 	}
 	
-	public static boolean detectCycleUtil(int s, boolean[] visited){
-	    if(visited[s] == true)
-		    return true;
-		visited[s] = true;
+	public static boolean detectCycleUtil(int s, int parent, boolean[] visited){
+	    visited[s] = true;
 		Iterator<Integer> i = adj[s].iterator();
 		while(i.hasNext()){
 		    int t = i.next();
-			if(visited[t] == true)
-			   return true;
-			if(detectCycleUtil(t, visited))
-				return true;
+			if(visited[t] != true){
+			    if(detectCycleUtil(t, s, visited)==true)
+				    return true;
+			}else{
+			    if(t != parent){
+				    return true;
+				}
+			}
 		}
 		return false;
 	}
 	
-	public static boolean detectCycle(int s){
+	public static boolean detectCycle(){
 	    boolean[] visited = new boolean[vertices];
-		return detectCycleUtil(s, visited);
+		
+		for(int v = 0; v < vertices; v++){
+		    if(visited[v] == false){
+			    if(detectCycleUtil(v, -1, visited)==true)
+				    return true;
+			}
+		}
+		
+		return false;
 	}
 	
     public static void main(String[] args){
@@ -41,10 +52,10 @@ public class GraphCycleDetection{
 		addEdge(0,1);
 		addEdge(0,2);
 		addEdge(1,2);
-		addEdge(2,0);
+		//addEdge(2,0);
 		addEdge(2,3);
-		addEdge(3,3);
-		System.out.println(detectCycle(2));
+		//addEdge(3,3);
+		System.out.println(detectCycle());
 	}
 
 	
